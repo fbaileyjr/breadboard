7941d94d6db841c6d9d4031a77475def50d199d7
Frank Bailey <frankbaileyjr@gmail.com>
2021-03-15 12:33:37 -0400

    created flash.py

---
+++ flash.py
@@ -0,0 +1,23 @@
+# import necessary libraries
+import RPi.GPIO as GPIO, time
+
+# define a function to turn the light on then off
+def blinkOnce(pin):
+    GPIO.output(pin, True)
+    time.sleep(1)
+    GPIO.output(pin, False)
+    time.sleep(1)
+
+
+# initialize the GPIO
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(17, GPIO.OUT)
+
+
+# use blinkOnce function in a loop
+for i in range(10):
+    blinkOnce(17)
+
+
+# cleanup
+GPIO.cleanup()
